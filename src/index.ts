01dcda8f401cf8e60be423e77afc8a80e0336180
sahil <sahil27raj@gmail.com>
2024-02-29 00:32:39 +0530

    add the changes in the code due to error

--- src/index.ts
+++ src/index.ts
@@ -10,20 +10,33 @@ interface CountryData {
   states: StateData[];
 }
 
-const data: CountryData[] = JSON.parse(fs.readFileSync('./data.json', 'utf-8'));
+const dataFilePath = './data.json';
 
-function getCountries(): string[] {
+async function readDataFile(): Promise<CountryData[]> {
+  try {
+    const fileContent = await fs.promises.readFile(dataFilePath, 'utf-8');
+    return JSON.parse(fileContent) as CountryData[];
+  } catch (error) {
+    console.error('Error reading or parsing data file:', error);
+    return [];
+  }
+}
+
+async function getCountries(): Promise<string[]> {
+  const data = await readDataFile();
   return data.map(countryData => countryData.country);
 }
 
-function getStatesByCountry(countryName: string): string[] {
+async function getStatesByCountry(countryName: string): Promise<string[]> {
+  const data = await readDataFile();
   const countryData = data.find(countryData => countryData.country === countryName);
   return countryData ? countryData.states.map(stateData => stateData.state) : [];
 }
 
-function getCitiesByState(stateName: string, countryName: string): string[] {
+async function getCitiesByState(stateName: string, countryName: string): Promise<string[]> {
+  const data = await readDataFile();
   const countryData = data.find(countryData => countryData.country === countryName);
-
+  
   if (countryData) {
     const stateData = countryData.states.find(stateData => stateData.state === stateName);
     return stateData ? stateData.cities : [];
